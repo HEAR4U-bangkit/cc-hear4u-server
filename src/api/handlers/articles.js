efe85d31c068615a171127af855fae24103a146b
dafariski77 <rskdafa@gmail.com>
2024-05-28 21:54:01 +0700

    chore: add basic handlers for article

---
+++ src/api/handlers/articles.js
@@ -0,0 +1,9 @@
+const getAllArticles = (request, h) => {
+  // findMany
+};
+
+const getOneArticle = (request, h) => {
+  // id didapat dari request.params.id
+};
+
+module.exports = { getAllArticles, getOneArticle };
